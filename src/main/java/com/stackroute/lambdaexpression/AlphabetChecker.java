d7f452d52c64679a6d8b85b8d2d4145b887b2c2c
Noel John Robert <noel060202@gmail.com>
2024-04-20 14:58:01 +0530

    completed

--- src/main/java/com/stackroute/lambdaexpression/AlphabetChecker.java
+++ src/main/java/com/stackroute/lambdaexpression/AlphabetChecker.java
@@ -3,8 +3,18 @@ package com.stackroute.lambdaexpression;
 import java.util.List;
 
 public class AlphabetChecker {
-    //write logic to find whether given string contains only alphabets or not
+    // write logic to find whether given string contains only alphabets or not
     public String checkAlphabets(List<String> inputList) {
-        return null;
+        // check if list is empty
+        if (inputList.isEmpty()) {
+            return "Give proper input not empty list";
+        }
+
+        // Check if all strings are alphabetic
+        boolean allAlphabetic = inputList.stream().allMatch(
+                str -> str.chars().allMatch(Character::isLetter)
+                );
+
+        return allAlphabetic ? "Given list contains only alphabet strings" : "Given list contains non alphabet strings";
     }
 }
